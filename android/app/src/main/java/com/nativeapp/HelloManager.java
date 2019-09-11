6e14936c843fd4ac062b90d67430c1a659b308b1
zidniryi <zidnirydwan@gmail.com>
2019-09-11 22:29:25 +0700

    install lib and create nativemodules

---
+++ android/app/src/main/java/com/nativeapp/HelloManager.java
@@ -0,0 +1,4 @@
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
