1bbe580bd9e127642cd9196530bcb1b98283b200
zidniryi <zidnirydwan@gmail.com>
2019-09-11 22:34:43 +0700

    create manager

--- android/app/src/main/java/com/nativeapp/HelloManager.java
+++ android/app/src/main/java/com/nativeapp/HelloManager.java
@@ -1,4 +1,16 @@
 import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+public class HelloManager extends ReactContextBaseJavaModule {
+    public HelloManager(ReactApplicationContext reactContext){
+        super(reactContext);
+    }
+
+    @override
+    public String getName() {
+        return "HelloManager";
+    }
+}
+
