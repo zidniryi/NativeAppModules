0d607b9e8d7bbfe3dc71649b19081d9c67a6030b
zidniryi <zidnirydwan@gmail.com>
2019-09-11 23:39:34 +0700

    finihsing the app

--- android/app/src/main/java/com/nativeapp/MainApplication.java
+++ android/app/src/main/java/com/nativeapp/MainApplication.java
@@ -2,7 +2,7 @@ package com.nativeapp;
 
 import android.app.Application;
 import android.util.Log;
-
+import com.nativeapp.CustomToastPackage;
 import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
@@ -27,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new CustomToastPackage()); // <-- Add this line with your package name.
       return packages;
     }
 
